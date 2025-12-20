fa338851d69d8596e6fbae21670c894d1033a845
omelette6764 <dabeatrizml@gmail.com>
2025-12-19 22:12:40 -0500

    Add PassportModule to AuthModule imports

--- apps/api/src/modules/auth/auth.module.ts
+++ apps/api/src/modules/auth/auth.module.ts
@@ -1,5 +1,6 @@
 import { Module } from "@nestjs/common";
 import { JwtModule } from "@nestjs/jwt";
+import { PassportModule } from "@nestjs/passport";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
 import { JwtStrategy } from "../../common/auth/jwt.strategy";
@@ -7,6 +8,7 @@ import { env } from "../../config/env";
 
 @Module({
   imports: [
+    PassportModule.register({ defaultStrategy: "jwt" }),
     JwtModule.register({
       secret: env().JWT_SECRET,
       signOptions: { expiresIn: env().JWT_ACCESS_TTL_SECONDS },
@@ -14,6 +16,6 @@ import { env } from "../../config/env";
   ],
   controllers: [AuthController],
   providers: [AuthService, JwtStrategy],
-  exports: [AuthService],
+  exports: [AuthService, PassportModule],
 })
 export class AuthModule {}
