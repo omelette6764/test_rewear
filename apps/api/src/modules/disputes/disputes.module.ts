0363d2f5392145be07e7b24b4801695e5f1651c2
omelette6764 <dabeatrizml@gmail.com>
2025-12-25 19:06:10 -0500

    Add DisputesModule with DisputesService provider

--- apps/api/src/modules/disputes/disputes.module.ts
+++ apps/api/src/modules/disputes/disputes.module.ts
@@ -1,1 +1,8 @@
+import { Module } from "@nestjs/common";
+import { DisputesService } from "./disputes.service";
 
+@Module({
+  providers: [DisputesService],
+  exports: [DisputesService],
+})
+export class DisputesModule {}
